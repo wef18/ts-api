c466e83c5cf2b3b51045d85a9d7d3ee7eeec5bbe
Wei Erfei <wef_1020@163.com>
2019-05-06 11:10:18 +0800

    ...

--- ts.sql
+++ ts.sql
@@ -124,11 +124,11 @@ INSERT INTO nursery_rhyme VALUES
   (NULL, '一闪一闪亮晶晶', 'yishanyishan.mp3', 'yishanyishan.jpg');
 INSERT INTO nursery_rhyme VALUES
   (NULL, 'Three Sick Kittens', 'ThreeSickKittens.mp3', 'batamu.jpg', 133),
-  (NULL, 'Three Little Kittens', 'ThreeLittleKittens.mp3', 'batamu.jpg', 148),
-  (NULL, 'Sing the Alphabet', 'SingtheAlphabet.mp3', 'batamu.jpg', 151),
-  (NULL, 'This Little Piggy', 'ThisLittlePiggy.mp3', 'batamu.jpg', 106),
-  (NULL, 'Rise and Shine', 'RiseandShine.mp3', 'batamu.jpg', 109),
-  (NULL, 'Rain Rain Go Away', 'RainRainGoAway.mp3', 'batamu.jpg', 126)
+  (NULL, 'Three Little Kittens', 'ThreeLittleKittens.mp3', 'batamu1.jpg', 148),
+  (NULL, 'Sing the Alphabet', 'SingtheAlphabet.mp3', 'batamu2.jpg', 151),
+  (NULL, 'This Little Piggy', 'ThisLittlePiggy.mp3', 'batamu3.jpg', 106),
+  (NULL, 'Rise and Shine', 'RiseandShine.mp3', 'batamu4.jpg', 109),
+  (NULL, 'Rain Rain Go Away', 'RainRainGoAway.mp3', 'batamu5.jpg', 126);
 
 /**** 故事音频 ****/
 INSERT INTO story VALUES
